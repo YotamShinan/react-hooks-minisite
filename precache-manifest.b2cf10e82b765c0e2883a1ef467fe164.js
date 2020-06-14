66343728d30bb7c3675cc5c086dfcfd61352abff
YotamShinan <yshinan@gmail.com>
2020-06-14 19:51:37 +0300

    Updates

---
+++ precache-manifest.b2cf10e82b765c0e2883a1ef467fe164.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086f08b435d13b2dcc27d04ee6e9c90a",
+    "url": "/react-hooks-minisite/index.html"
+  },
+  {
+    "revision": "6ddf64e403c443961f75",
+    "url": "/react-hooks-minisite/static/css/main.a5001bc4.chunk.css"
+  },
+  {
+    "revision": "a5b85f41604efb69160c",
+    "url": "/react-hooks-minisite/static/js/2.396df124.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-minisite/static/js/2.396df124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ddf64e403c443961f75",
+    "url": "/react-hooks-minisite/static/js/main.e45fac3c.chunk.js"
+  },
+  {
+    "revision": "7b7c8b27a31167e20013",
+    "url": "/react-hooks-minisite/static/js/runtime-main.9c62abd0.js"
+  },
+  {
+    "revision": "5314dab9b932b65cc40c86ef69aff264",
+    "url": "/react-hooks-minisite/static/media/Aclonica-Regular.5314dab9.ttf"
+  },
+  {
+    "revision": "ba13edfb3ac4816c9069a1639e896bf0",
+    "url": "/react-hooks-minisite/static/media/Birly-0WDBv.ba13edfb.otf"
+  },
+  {
+    "revision": "9ccb885c9cf8e503d557f15e0b2cbf24",
+    "url": "/react-hooks-minisite/static/media/Merriweather-Bold.9ccb885c.ttf"
+  },
+  {
+    "revision": "63e1f06ea6bf31c8f3143c143e7e6c2a",
+    "url": "/react-hooks-minisite/static/media/Merriweather-BoldItalic.63e1f06e.ttf"
+  },
+  {
+    "revision": "498bf4ee4ac9fab22ad9f814839173b2",
+    "url": "/react-hooks-minisite/static/media/Merriweather-Italic.498bf4ee.ttf"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/react-hooks-minisite/static/media/Merriweather-Regular.c97a9fc2.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/react-hooks-minisite/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "0e7e9a9b5c4abaadef7bc8f4e4574084",
+    "url": "/react-hooks-minisite/static/media/Segoe-UI.0e7e9a9b.ttf"
+  },
+  {
+    "revision": "8ec04ac829a7ea0d0b4d684e40e46a80",
+    "url": "/react-hooks-minisite/static/media/Teko-Bold.8ec04ac8.ttf"
+  },
+  {
+    "revision": "8e22c2a6e3a3c679787e763a97fa11f7",
+    "url": "/react-hooks-minisite/static/media/Ubuntu-Medium.8e22c2a6.ttf"
+  }
+]);
